b5a0a9cc727eb983c8fcb60ee804812eff27675e
vandoanstore <keokisesslerrev@outlook.fr>
2025-07-27 16:27:41 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@
 <html lang="vi">
 <head>
   <meta charset="UTF-8" />
-  <link rel="icon" type="image/png" href="/VDstore/favicon.png" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title> Chăn ga gối đệm</title>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
@@ -10,6 +9,6 @@
 </head>
 <body>
   <div id="root"></div>
-  <script type="module" src="/VDstore/src/main.jsx"></script>
+  <script type="module" src="/src/main.jsx"></script>
 </body>
 </html>
