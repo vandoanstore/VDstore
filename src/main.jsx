86dbdb44db2ddf5e75c095b8a0863cf0c0349eab
vandoanstore <keokisesslerrev@outlook.fr>
2025-07-27 16:23:31 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './style.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+  <meta charset="UTF-8" />
+  <link rel="icon" type="image/png" href="/VDstore/favicon.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title> Chăn ga gối đệm</title>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>
+<body>
+  <div id="root"></div>
+  <script type="module" src="/VDstore/src/main.jsx"></script>
+</body>
+</html>
